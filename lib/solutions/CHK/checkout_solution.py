fa0bef277449c323d3ebc1c657a9dbbbdccf61b5
sbx_user1051 <sbx_user1051@169.254.93.255>
2025-10-30 12:05:45 +0000

    Thu Oct 30 12:05:45 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -10,20 +10,35 @@ _item_prices = {
 _items = _item_prices.keys()
 
 
+class SpecialOffer:
+    def __init__(self, count_of_items: int, special_price: int) -> None:
+        self.count_of_items = count_of_items
+        self.special_price = special_price
+
+
 class CheckoutSolution:
     # skus = unicode string
     def checkout(self, skus: str) -> int:
-        for sku in skus:
-            if sku not in _items:
-                return -1
+        if not self._skus_valid(skus):
+            return -1
 
         item_counts = Counter(skus)
-
         total = 0
+        for item, count in item_counts.items():
+            ...
+
+        # Calculate remainders
         for item, count in item_counts.items():
             item_price = _item_prices[item]
             total += item_price * count
 
         return total
+
+    def _skus_valid(self, skus: str) -> bool:
+        for sku in skus:
+            if sku not in _items:
+                return False
+        return True
+
 
 
