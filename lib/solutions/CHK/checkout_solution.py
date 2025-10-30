56853b61232599ec8e18294bc34d755d61c92435
sbx_user1051 <sbx_user1051@169.254.93.255>
2025-10-30 12:02:45 +0000

    Thu Oct 30 12:02:45 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,3 +1,4 @@
+from collections import Counter
 
 _item_prices = {
     "A": 50,
@@ -6,11 +7,23 @@ _item_prices = {
     "D": 15,
 }
 
+_items = _item_prices.keys()
+
 
 class CheckoutSolution:
     # skus = unicode string
     def checkout(self, skus: str) -> int:
+        for sku in skus:
+            if sku not in _items:
+                return -1
+
         item_counts = Counter(skus)
 
-        raise NotImplementedError()
+        total = 0
+        for item, count in item_counts.items():
+            item_price = _item_prices[item]
+            total += item_price * count
+
+        return total
+
 
