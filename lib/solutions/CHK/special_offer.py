eb2be06619459cec2faaedb92434c32d39378795
sbx_user1051 <sbx_user1051@169.254.31.193>
2025-10-30 16:35:49 +0000

    Thu Oct 30 16:35:49 UTC 2025

--- lib/solutions/CHK/special_offer.py
+++ lib/solutions/CHK/special_offer.py
@@ -8,22 +8,32 @@ class SpecialOffer(ABC):
 
 
 class MultiBuyOffer(SpecialOffer):
-    def __init__(self, item: str, count_of_items: int, price: int) -> None:
-        super().__init__(item, count_of_items)
+    def __init__(self, item: str, num_items_to_qualify: int, price: int) -> None:
+        super().__init__(item, num_items_to_qualify)
         self.price = price
+
+    def __str__(self) -> str:
+        return f"{self.num_items_to_qualify}{self.item} for {self.price}"
 
 
 class BuyAndGetFreeOffer(SpecialOffer):
-    def __init__(self, item: str, count_of_items: int, free_item: str) -> None:
-        super().__init__(item, count_of_items)
+    def __init__(self, item: str, num_items_to_qualify: int, free_item: str) -> None:
+        super().__init__(item, num_items_to_qualify)
         self.free_item = free_item
+
+    def __str__(self) -> str:
+        return f"{self.num_items_to_qualify}{self.item} get one {self.free_item} free"
 
 
 PRIORITISED_SPECIAL_OFFERS = [
-    MultiBuyOffer(item="A", count_of_items=5, price=200),
-    MultiBuyOffer(item="A", count_of_items=3, price=130),
-    BuyAndGetFreeOffer(item="E", count_of_items=2, free_item="B"),
-    MultiBuyOffer(item="B", count_of_items=2, price=45),
-    MultiBuyOffer(item="F", count_of_items=3, price=20),
+    MultiBuyOffer(item="A", num_items_to_qualify=5, price=200),
+    MultiBuyOffer(item="A", num_items_to_qualify=3, price=130),
+    BuyAndGetFreeOffer(item="E", num_items_to_qualify=2, free_item="B"),
+    MultiBuyOffer(item="B", num_items_to_qualify=2, price=45),
+    MultiBuyOffer(item="F", num_items_to_qualify=3, price=20),
+    MultiBuyOffer(item="H", num_items_to_qualify=10, price=80),
+    MultiBuyOffer(item="H", num_items_to_qualify=5, price=45),
+    MultiBuyOffer(item="K", num_items_to_qualify=2, price=150),
 ]
+
 
