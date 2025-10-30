778aaf5abbf42a2dcc42cbc2709637ac2d7cb753
sbx_user1051 <sbx_user1051@169.254.73.137>
2025-10-30 11:25:15 +0000

    Thu Oct 30 11:25:15 UTC 2025

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -1,12 +1,7 @@
 class HelloSolution:
     # friend_name = unicode string
     def hello(self, friend_name: str | None) -> str:
-        if friend_name is None:
-            raise ValueError("friend_name cannot be None")
+        return "Hello, World!"
 
-        if len(friend_name) > 100:
-            raise ValueError("friend_name must be less than 100")
-
-        return f'Hello, {friend_name}!'
 
 
--- test/solution_tests/HLO/test_hello_solution.py
+++ test/solution_tests/HLO/test_hello_solution.py
@@ -1,19 +1,12 @@
-import pytest
 from solutions.HLO.hello_solution import HelloSolution
 
 
 class TestHello:
     def test_hello(self) -> None:
-        assert HelloSolution().hello("Harry") == "Hello, Harry!"
+        assert HelloSolution().hello("Craftsman") == "Hello, World!"
 
-    def test_raise_error_when_name_none(self) -> None:
-        with pytest.raises(ValueError):
-            HelloSolution().hello(None)
+    def test_hello_mr_x(self) -> None:
+        assert HelloSolution().hello("Mr. X") == "Hello, World!"
 
-    def test_raise_error_when_name_longer_than_100_chars(self) -> None:
-        name = "a" * 101
-
-        with pytest.raises(ValueError):
-            HelloSolution().hello(name)
 
 
