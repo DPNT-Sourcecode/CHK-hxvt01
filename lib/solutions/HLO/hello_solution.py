5278332de4517ab157435fdd2f3e612004728401
sbx_user1051 <sbx_user1051@169.254.73.137>
2025-10-30 11:26:36 +0000

    Thu Oct 30 11:26:36 UTC 2025

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -1,7 +1,14 @@
 class HelloSolution:
     # friend_name = unicode string
     def hello(self, friend_name: str | None) -> str:
-        return "Hello, World!"
+        if friend_name is None:
+            raise ValueError("friend_name cannot be None")
+
+        if len(friend_name) > 100:
+            raise ValueError("friend_name must be less than 100")
+
+        return f"Hello, {friend_name}!"
+
 
 
 
--- test/solution_tests/HLO/test_hello_solution.py
+++ test/solution_tests/HLO/test_hello_solution.py
@@ -3,10 +3,11 @@ from solutions.HLO.hello_solution import HelloSolution
 
 class TestHello:
     def test_hello(self) -> None:
-        assert HelloSolution().hello("Craftsman") == "Hello, World!"
+        assert HelloSolution().hello("Craftsman") == "Hello, Craftsman!"
 
     def test_hello_mr_x(self) -> None:
-        assert HelloSolution().hello("Mr. X") == "Hello, World!"
+        assert HelloSolution().hello("Mr. X") == "Hello, Mr. X!"
+
 
 
 
